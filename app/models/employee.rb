10068f7bbb1e0eb901102bbb96ca8444a7a24c12
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 15:58:05 +0200

    feat: Add migration and validations to the Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,7 @@ class Employee < ApplicationRecord
   validates :name, presence: true
   validates :password, length: { in: 6..8 }
   with_options presence: true do
-    validates :user_name, uniqueness: true, format: { without: /\s/, message: ' must contain no spaces' }
+    validates :user_name, uniqueness: true, format: { without: /\s/, message: 'must contain no spaces' }
     validates :job_title, inclusion: { in: VALID_ROLES }
     with_options uniqueness: { case_sensitive: false } do
       validates :phone_number, format: { with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/, message: 'is invalid' }
