4cba22e5f724810d6e83ebbdfc81266c4a7195e1
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 14:41:22 +0200

    feat: Add validations to to the Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@
 
 class Item < ApplicationRecord
   has_one :inventory_level, dependent: :destroy
+  validates :name, :size, :color, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
