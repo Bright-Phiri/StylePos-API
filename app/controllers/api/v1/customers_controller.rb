19e557a5f0aacdaf47d278119806018a7f110033
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 16:24:27 +0200

    feat: Implement CRUD actions in Customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 class Api::V1::CustomersController < ApplicationController
-  
+  before_action :set_customer, only: [:update, :show, :destory]
+  def index
+    customers = Customer.all
+    render json: customers
+  end
+
+  def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: customer, status: :created
+    else
+      render json: customer.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone_number)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 end
