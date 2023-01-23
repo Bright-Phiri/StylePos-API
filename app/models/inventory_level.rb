edbcd87e28c2b0851ef69ed805b223b6cae148de
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 14:59:33 +0200

    feat: Add validations to to the Inventory Level model

--- app/models/inventory_level.rb
+++ app/models/inventory_level.rb
@@ -4,5 +4,5 @@ class InventoryLevel < ApplicationRecord
   belongs_to :item
   validates :supplier, presence: true
   validates :quantity, :reorder_level, numericality: { only_integer: true }
-  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level'}
+  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }
 end
