173019ec3516e9c6dbfc87dceb80ebb5f596fdd0
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 16:38:26 +0200

    feat: Finalize Customer features

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Api::V1::CustomersController < ApplicationController
-  before_action :set_customer, only: [:update, :show, :destory]
+  before_action :set_customer, only: [:update, :show, :destroy]
   def index
     customers = Customer.all
     render json: customers
+  end
+
+  def show
+    render json: @customer
   end
 
   def create
@@ -14,6 +18,19 @@ class Api::V1::CustomersController < ApplicationController
     else
       render json: customer.errors.full_messages, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @customer.update(customer_params)
+      render json: @customer, status: :ok
+    else
+      render json: @customer.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @customer.destroy!
+    head :no_content
   end
 
   private
