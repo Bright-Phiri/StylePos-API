50cb7fc484bb548009d21a2370da321f353a824f
Bright Phiri <bphiri.aki@gmail.com>
2023-01-23 16:42:27 +0200

    feat: Add validations to the Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@
 
 class Customer < ApplicationRecord
   has_many :orders
+  validates :name, presence: true
+  validates :phone_number, presence: true, format: { with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/, message: 'is invalid' }
 end
