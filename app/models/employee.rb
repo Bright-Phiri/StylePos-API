9952db81b012d1bc06b840ceb830569d24667f0d
Bright Phiri <bphiri.aki@gmail.com>
2023-01-24 08:29:12 +0200

    Fix: Fix phone number validation errors

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -11,7 +11,7 @@ class Employee < ApplicationRecord
     validates :user_name, uniqueness: true, format: { without: /\s/, message: 'must contain no spaces' }
     validates :job_title, inclusion: { in: VALID_ROLES }
     with_options uniqueness: { case_sensitive: false } do
-      validates phone_number: true
+      validates :phone_number, phone_number: true
       validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'is invalid' }
     end
   end
