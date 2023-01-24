8280e2c439775a1b2c28d20c3e74a6aba45d97ba
Bright Phiri <bphiri.aki@gmail.com>
2023-01-24 11:27:44 +0200

    feat: Add support for accepts_nested_attributes in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@
 class Order < ApplicationRecord
   has_many :line_items
   validates_associated :line_items
+  accepts_nested_attributes_for :line_items, allow_destroy: true
   belongs_to :customer
   belongs_to :employee
 end
