6a88057412c7b9ad32c0ccf439b43bff5b6dee35
Bright Phiri <bphiri.aki@gmail.com>
2023-01-24 08:20:43 +0200

    Refactor : Refactor Customer and Employee models to include phone number validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -11,7 +11,7 @@ class Employee < ApplicationRecord
     validates :user_name, uniqueness: true, format: { without: /\s/, message: 'must contain no spaces' }
     validates :job_title, inclusion: { in: VALID_ROLES }
     with_options uniqueness: { case_sensitive: false } do
-      validates :phone_number, format: { with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/, message: 'is invalid' }
+      validates phone_number: true
       validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'is invalid' }
     end
   end
