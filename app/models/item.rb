735af36e1609ec8d7e743a72b3beaa84a6f3bf44
Bright Phiri <bphiri.aki@gmail.com>
2023-01-24 11:39:44 +0200

    feat: Add has_many :line_items association to Item  model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@
 
 class Item < ApplicationRecord
   has_one :inventory_level, dependent: :destroy
+  has_many :line_items
   validates_associated :inventory_level
   validates :name, :size, :color, presence: true
   validates :price, numericality: { greater_than: 0 }
