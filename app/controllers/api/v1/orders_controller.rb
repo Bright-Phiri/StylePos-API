65fad7fe2be0d8bbcfdabe8571b555586336f08b
Bright Phiri <bphiri.aki@gmail.com>
2023-01-27 11:02:35 +0200

    Revert "feat: Add more order features"
    This reverts commit 68fdb33152f083910040864be7b5e35f485fb388.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,33 +5,4 @@ class Api::V1::OrdersController < ApplicationController
     orders = Order.all
     render json: orders
   end
-
-  def add_line_item
-    @line_items = []
-    if @line_item.save
-      @line_items << @line_item
-      render json: @line_items
-    else
-      render json: @line_item.errors.full_messages, status: :unprocessable_entity
-    end
-  end
-
-  def find_item
-    @item = Item.find(params[:item_id])
-    render json: @item
-  end
-
-  def set_total_price
-    @line_item = @item.line_items.build(line_item_params.merge(price: @item.price))
-    @line_item.total = @line_item.quantity * @line_item.price
-    render json: @line_item
-  end
-
-  def order_params
-    params.require(:order).permit(:customer_name, line_items_attributes: [:item_id, :quantity, :price, :total])
-  end
-
-  def line_item_params
-    params.require(:line_item).permit(:item_id, :quantity, :price, :total)
-  end
 end
--- db/migrate/20230126142017_add_total_to_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddTotalToLineItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :line_items, :total, :decimal
-  end
-end
