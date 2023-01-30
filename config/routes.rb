b5aca54f7896e32e433358966f1d3ac0abe89113
Bright Phiri <bphiri.aki@gmail.com>
2023-01-30 10:40:58 +0200

    feat: Update endpoints for orders

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
         resources :inventory_levels, except: [:index, :destory]
       end
       resources :inventory_levels, only: [:index, :destroy]
-      resources :orders, only: :index
+      resources :orders, only: [:index, :show]
       resources :customers
       resources :employees do
-        resources :orders, except: :index do
+        resources :orders, except: [:index, :show] do
           post 'add_line_item', action: :add_line_item, controller: 'orders'
           delete 'remove_line_item/:id', action: :remove_line_item, controller: 'orders'
           post 'set_total_price', action: :set_total_price, controller: 'orders'
