f4fc7f5db08fddcd339a3dbb351bbecf2d1ce1fe
Bright Phiri <bphiri.aki@gmail.com>
2023-02-05 20:25:32 +0200

    fix: Fix validations in Inventory Level model

--- app/models/inventory_level.rb
+++ app/models/inventory_level.rb
@@ -3,8 +3,8 @@
 class InventoryLevel < ApplicationRecord
   belongs_to :item
   validates :supplier, presence: true, allow_blank: true
-  validates :quantity, :reorder_level, numericality: { greater_than: 0, only_integer: true }, on: :create
-  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }, on: :create
+  validates :quantity, :reorder_level, numericality: { greater_than: 0, only_integer: true }
+  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }
   after_save :check_inventory_level
 
   def check_inventory_level
