f3c4d55976facdcbc42b3dec4c02119495e97018
Bright Phiri <bphiri.aki@gmail.com>
2023-02-20 12:20:25 +0200

    feat: Update Inventory Level and Item Validations

--- app/models/inventory_level.rb
+++ app/models/inventory_level.rb
@@ -4,7 +4,7 @@ class InventoryLevel < ApplicationRecord
   belongs_to :item
   validates :supplier, presence: true, allow_blank: true
   validates :quantity, :reorder_level, numericality: { greater_than: 0, only_integer: true }
-  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }
+  validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }, on: :create
   after_save :check_inventory_level
 
   def check_inventory_level
