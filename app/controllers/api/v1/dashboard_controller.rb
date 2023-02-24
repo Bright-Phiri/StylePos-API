464b1bef7ba8cbae1adbe02922d1f8782f8e5315
Bright Phiri <bphiri.aki@gmail.com>
2023-02-24 13:57:29 +0200

    Refactor: Refactor dashboard controller

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -2,17 +2,17 @@
 
 class Api::V1::DashboardController < ApplicationController
   def index
-    number_of_orders = Order.of_day.count
-    total_sales = Order.sum(:total)
-    daily_sales = Order.daily_revenue.sum(:total)
-    weekly_revenue = Order.weekly_revenue.sum(:total)
-    monthly_revenue = Order.monthly_revenue.sum(:total)
-    fastest_moving_items = Item.best_selling.limit(10)
-    slowest_moving_items = Item.slow_moving.limit(10)
-    monthly_order_statistics = Order.statistics
-    items_out_of_stock_count = Item.out_of_stock.count
-    items_in_stock_count = Item.in_stock.count
-
-    render json: { items_in_stock_count:, items_out_of_stock_count:, number_of_orders:, total_sales:, fastest_moving_items:, slowest_moving_items:, monthly_order_statistics:, daily_sales:, weekly_revenue:, monthly_revenue: }
+    render json: {
+      number_of_orders: Order.of_day.count,
+      total_sales: Order.sum(:total),
+      daily_sales: Order.daily_revenue.sum(:total),
+      weekly_revenue: Order.weekly_revenue.sum(:total),
+      monthly_revenue: Order.monthly_revenue.sum(:total),
+      fastest_moving_items: Item.best_selling.limit(10),
+      slowest_moving_items: Item.slow_moving.limit(10),
+      monthly_order_statistics: Order.statistics,
+      items_out_of_stock_count: Item.out_of_stock.count,
+      items_in_stock_count: Item.in_stock.count
+    }
   end
 end
