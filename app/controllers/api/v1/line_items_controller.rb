ea5fb1feca6b5c7a5c4e238fdc22c56bbe53d069
Bright Phiri <bphiri.aki@gmail.com>
2023-03-01 23:22:58 +0200

    feat: Update line items controller

--- app/controllers/api/v1/line_items_controller.rb
+++ app/controllers/api/v1/line_items_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::V1::LineItemsController < ApplicationController
-  before_action :set_line_item, only: [:set_total_price]
-
   def create
     order = Order.find(params[:order_id])
     item = Item.preload(:inventory_level).find(params[:line_item][:item_id])
@@ -26,19 +24,9 @@ class Api::V1::LineItemsController < ApplicationController
     render json: { vat: order.total_vat, order_total: order.total, sub_total:, line_items: LineItemsRepresenter.new(order.line_items.reload).as_json }, status: :ok
   end
 
-  def set_total_price
-    render json: LineItemRepresenter.new(@line_item).as_json
-  end
-
   private
 
   def line_item_params
     params.require(:line_item).permit(:item_id, :quantity, :price)
-  end
-
-  def set_line_item
-    @item = Item.find(params[:line_item][:item_id])
-    @line_item = @item.line_items.build(line_item_params.merge(price: @item.price))
-    @line_item.total = params[:line_item][:quantity] * @line_item.price
   end
 end
