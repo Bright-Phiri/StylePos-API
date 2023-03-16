729e7ab4c95f3e7efdcf9dd5dc652df6950523ef
Bright Phiri <bphiri.aki@gmail.com>
2023-03-17 01:42:22 +0200

    fix: Bug fix on adding line item to order

--- app/controllers/api/v1/line_items_controller.rb
+++ app/controllers/api/v1/line_items_controller.rb
@@ -6,25 +6,12 @@ class Api::V1::LineItemsController < ApplicationController
     item = Item.preload(:inventory_level).find(params[:line_item][:item_id])
     raise ExceptionHandler::InventoryLevelError, 'Not enough inventory' unless item.enough_inventory?(params[:line_item][:quantity].to_i)
 
-    vat = LineItem.calculate_vat(item.price, params[:line_item][:quantity].to_i)
-    line_item = order.line_items.create(line_item_params.merge(item_id: item.id, price: item.price + vat, vat:, total: params[:line_item][:quantity].to_i * (item.price + vat)))
+    vat = LineItem.calculate_vat(item.price)
+    line_item = order.line_items.create(line_item_params.merge(item_id: item.id, price: item.price + vat, vat: vat * params[:line_item][:quantity].to_i, total: params[:line_item][:quantity].to_i * (item.price + vat)))
     if line_item.new_record?
       render json: line_item.errors.full_messages, status: :unprocessable_entity
     else
       render json: OrderRepresenter.new(order).as_json, status: :created
-    end
-  end
-
-  def update
-    order = Order.find(params[:order_id])
-    line_item = order.line_items.find(params[:id])
-    raise ExceptionHandler::InventoryLevelError, 'Not enough inventory' unless line_item.item.enough_inventory?(params[:line_item][:quantity].to_i)
-
-    vat = LineItem.calculate_vat(line_item.item.price, params[:line_item][:quantity].to_i)
-    if line_item.update(price: line_item.item.price + vat, quantity: params[:line_item][:quantity], vat:, total: params[:line_item][:quantity].to_i * (line_item.item.price + vat))
-      render json: OrderRepresenter.new(order).as_json, status: :ok
-    else
-      render json: line_item.errors.full_messages, status: :unprocessable_entity
     end
   end
 
