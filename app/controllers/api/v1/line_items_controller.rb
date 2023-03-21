2af8692c3dd9d2ae6e25512320bb623e221d08c3
Bright Phiri <bphiri.aki@gmail.com>
2023-03-21 10:20:49 +0200

    Refactor: Refactor add and update line items features

--- app/controllers/api/v1/line_items_controller.rb
+++ app/controllers/api/v1/line_items_controller.rb
@@ -5,28 +5,12 @@ class Api::V1::LineItemsController < ApplicationController
   def create
     order = Order.find(params[:order_id])
     item = Item.preload(:inventory_level).find(params[:line_item][:item_id])
-    raise ExceptionHandler::InventoryLevelError, 'Not enough inventory' unless item.enough_inventory?(params[:line_item][:quantity].to_i)
-
-    vat = LineItem.calculate_vat(item.price, params[:line_item][:quantity].to_i)
-    line_item = order.line_items.create(line_item_params.merge(item_id: item.id, price: item.price + vat, vat:, total: LineItem.calculate_total(item.price, params[:line_item][:quantity].to_i)))
-    if line_item.new_record?
-      render json: line_item.errors.full_messages, status: :unprocessable_entity
-    else
-      render json: OrderRepresenter.new(order).as_json, status: :created
-    end
+    create_or_update_line_item(order, item)
   end
 
   def update
     @line_item.destroy! unless params[:line_item][:quantity].to_i <= 0
-    raise ExceptionHandler::InventoryLevelError, 'Not enough inventory' unless @line_item.item.enough_inventory?(params[:line_item][:quantity].to_i)
-
-    vat = LineItem.calculate_vat(@line_item.item.price, params[:line_item][:quantity].to_i)
-    line_item = @order.line_items.create(line_item_params.merge(item_id: @line_item.item.id, price: @line_item.item.price + vat, vat:, total: LineItem.calculate_total(@line_item.item.price, params[:line_item][:quantity].to_i)))
-    if line_item.new_record?
-      render json: line_item.errors.full_messages, status: :unprocessable_entity
-    else
-      render json: OrderRepresenter.new(@order).as_json, status: :ok
-    end
+    create_or_update_line_item(@line_item.order, @line_item.item)
   end
 
   def destroy
@@ -43,5 +27,17 @@ class Api::V1::LineItemsController < ApplicationController
   def set_line_item
     @order = Order.preload(:line_items).find(params[:order_id])
     @line_item = @order.line_items.find(params[:id])
+  end
+
+  def create_or_update_line_item(order, item)
+    raise ExceptionHandler::InventoryLevelError, 'Not enough inventory' unless item.enough_inventory?(params[:line_item][:quantity].to_i)
+
+    vat = LineItem.calculate_vat(item.price, params[:line_item][:quantity].to_i)
+    line_item = order.line_items.create(line_item_params.merge(item_id: item.id, price: item.price + vat, vat:, total: LineItem.calculate_total(item.price, params[:line_item][:quantity].to_i)))
+    if line_item.new_record?
+      render json: line_item.errors.full_messages, status: :unprocessable_entity
+    else
+      render json: OrderRepresenter.new(order).as_json, status: :ok
+    end
   end
 end
