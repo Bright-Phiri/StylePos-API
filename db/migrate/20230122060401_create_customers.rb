30d03ebe87977766eed6bba005cd4ef6b8cb7416
Bright Phiri <bphiri.aki@gmail.com>
2023-04-06 10:34:42 +0200

    Feat: Add phone_number column with unique index

--- db/migrate/20230122060401_create_customers.rb
+++ db/migrate/20230122060401_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
       t.string :name
-      t.string :phone_number
+      t.string :phone_number, index: { unique: true }
 
       t.timestamps
     end
