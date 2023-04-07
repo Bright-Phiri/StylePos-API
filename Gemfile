a2b0f3a4b3b3756a4f8d6b7908675149ec7a0cb8
Bright Phiri <bphiri.aki@gmail.com>
2023-04-07 17:32:42 +0200

    feat: Add pagination to API response

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "bootsnap", require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem "rack-cors"
+gem 'csv', '~> 3.0'
+gem 'will_paginate', '~> 3.1.1'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/api/v1/inventory_levels_controller.rb
+++ app/controllers/api/v1/inventory_levels_controller.rb
@@ -3,8 +3,8 @@
 class Api::V1::InventoryLevelsController < ApplicationController
   before_action :set_inventory_level, only: [:update, :show]
   def index
-    inventory_levels = InventoryLevel.preload(:item).all
-    render json: InventoriesRepresenter.new(inventory_levels).as_json
+    inventory_levels = InventoryLevel.preload(:item).paginate(page: params[:page], per_page: params[:per_page])
+    render json: { inventory_levels: InventoriesRepresenter.new(inventory_levels).as_json, total: inventory_levels.total_entries }
   end
 
   def show
