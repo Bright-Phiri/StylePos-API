51941dced78927ba3bd69ad21a909cd1edaf89f0
Bright Phiri <bphiri.aki@gmail.com>
2023-04-11 09:29:46 +0200

    feat: Add search scope in Item model

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ClothingStoresApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.elasticsearch.url = ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'
 
     # Configuration for the application, engines, and railties goes here.
     #
