ce5dceb440850cff9f4e6fddfb29fbc1896c65b9
Bright Phiri <bphiri.aki@gmail.com>
2023-05-02 14:34:12 +0200

    feat: Update end-points

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
+
 require 'sidekiq/web'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   mount ActionCable.server => '/cable'
   mount Sidekiq::Web => '/sidekiq'
   namespace :api do
