fa6ed15305a21f4c3a218292439aacfda7fd0ab2
Bright Phiri <bphiri.aki@gmail.com>
2023-05-03 09:44:45 +0200

    fix: Enable valid Rack session for CSRF protection

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module ClothingStoresApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.active_job.queue_adapter     = :sidekiq
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
