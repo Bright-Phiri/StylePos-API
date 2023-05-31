e7bc01696bac383a2f963cb3d5a393763fd26027
Bright Phiri <bphiri.aki@gmail.com>
2023-05-31 16:20:54 +0200

    fix: Fix foreign key constraint on the item column

--- db/migrate/20230123091136_create_inventory_levels.rb
+++ db/migrate/20230123091136_create_inventory_levels.rb
@@ -4,7 +4,7 @@ class CreateInventoryLevels < ActiveRecord::Migration[7.0]
       t.integer :quantity
       t.integer :reorder_level
       t.string :supplier
-      t.belongs_to :item, null: false, foreign_key: true
+      t.belongs_to :item, index: { unique: true }, null: false, foreign_key: true
 
       t.timestamps
     end
