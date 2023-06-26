130cbaf0a8cc0f34155e7e0fb68e2a2d18803aee
Bright Phiri <bphiri.aki@gmail.com>
2023-06-26 09:43:40 +0200

    feat: Update end-points

--- config/routes.rb
+++ config/routes.rb
@@ -16,24 +16,24 @@ Rails.application.routes.draw do
       resources :inventory_levels, only: [:index, :destroy]
       resources :returns, only: :index
       resources :orders, only: [:index, :show, :destroy] do
-        post 'return_item/:line_item_id', action: :return_item, controller: 'returns'
+        post '/return_item/:line_item_id', action: :return_item, controller: 'returns'
       end
-      get 'find_item/:barcode', action: :find_item, controller: 'items'
+      get '/find_item/:barcode', action: :find_item, controller: 'items'
       resources :customers
-      get 'filter_transactions', action: :find, controller: 'orders'
+      get '/filter_transactions', action: :find, controller: 'orders'
       resources :employees do
         resources :orders, except: [:index, :show, :destory] do
           resources :line_items, only: [:create, :update, :destroy]
-          put 'apply_discount/:id', action: :apply_discount, controller: 'line_items'
+          put '/apply_discount/:id', action: :apply_discount, controller: 'line_items'
         end
       end
-      post 'disable_user/:id', action: :disable_user, controller: 'employees'
-      post 'activate_user/:id', action: :activate_user, controller: 'employees'
-      post 'login', action: :login, controller: 'authentication'
-      post 'register', action: :set_manager, controller: 'employees'
-      put 'update_password/:id', action: :change, controller: 'passwords'
-      post 'forgot_password', action: :forgot, controller: 'passwords'
-      post 'reset_password', action: :reset, controller: 'passwords'
+      post '/disable_user/:id', action: :disable_user, controller: 'employees'
+      post '/activate_user/:id', action: :activate_user, controller: 'employees'
+      post '/login', action: :login, controller: 'authentication'
+      post '/register', action: :set_manager, controller: 'employees'
+      put '/update_password/:id', action: :change, controller: 'passwords'
+      post '/forgot_password', action: :forgot, controller: 'passwords'
+      post '/reset_password', action: :reset, controller: 'passwords'
     end
   end
 end
