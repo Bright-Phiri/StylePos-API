7c0a669871275548ae50364128ebf11fd9da7fb0
Bright Phiri <bphiri.aki@gmail.com>
2023-07-18 11:35:47 +0200

    fix: Bug fix on category validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :items, dependent: :destory
+  has_many :items, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true, allow_blank: true
   attr_readonly :items_count
