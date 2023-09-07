50262f7eeac6836cdb7be999d941681eed1edec1
Bright Phiri <bphiri.aki@gmail.com>
2023-09-07 14:53:02 +0200

    fix: Fix model level validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,5 @@
 class Customer < ApplicationRecord
   validates_associated :orders
   validates :name, presence: true
-  validates :phone_number, presence: true, phone_number: true
+  validates :phone_number, presence: true, uniqueness: true, phone_number: true
 end
