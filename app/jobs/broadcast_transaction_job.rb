ba48a076c5c732ae547bf3480fae828e1ddea439
Bright Phiri <bphiri.aki@gmail.com>
2023-09-08 14:41:12 +0200

    Fix: Handle ActiveJob::DeserializationError gracefully

--- app/jobs/broadcast_transaction_job.rb
+++ app/jobs/broadcast_transaction_job.rb
@@ -3,6 +3,8 @@
 class BroadcastTransactionJob < ApplicationJob
   queue_as :transaction_broadcast
 
+  discard_on ActiveJob::DeserializationError
+
   def perform(transaction)
     txn = OrderRepresenter.new(transaction).as_json
     ActionCable.server.broadcast('customer_display_channel', { transaction: txn.to_json })
