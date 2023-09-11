24e73bf98bb63632eb8d654b8f2249c302b4a458
Bright Phiri <bphiri.aki@gmail.com>
2023-09-11 09:19:48 +0200

    feat: Update end-points

--- config/routes.rb
+++ config/routes.rb
@@ -9,35 +9,55 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   namespace :api do
     namespace :v1 do
-      get '/show_items/:id', action: :show_items, controller: 'categories'
       resources :categories do
+        collection do
+          get '/show_items/:id', action: :show_items, controller: 'categories'
+        end
         resources :items, except: [:index, :create, :destory] do
           resources :inventory_levels, except: [:index, :destory]
         end
       end
       resources :dashboard, only: :index
       resources :inventory_levels, only: [:index, :destroy]
-      resources :items, only: [:index, :create, :destroy]
-      resources :returns, only: :index
-      resources :orders, only: [:index, :show, :destroy] do
-        post '/return_item/:line_item_id', action: :return_item, controller: 'returns'
-      end
-      get '/find_item/:barcode', action: :find_item, controller: 'items'
-      resources :customers
-      get '/filter_transactions', action: :find, controller: 'orders'
-      resources :employees do
-        resources :orders, except: [:index, :show, :destory] do
-          resources :line_items, only: [:create, :update, :destroy]
-          put '/apply_discount/:id', action: :apply_discount, controller: 'line_items'
+      resources :items, only: [:index, :create, :destroy] do
+        collection do
+          get '/find_item/:barcode', action: :find_item, controller: 'items'
         end
       end
-      post '/disable_user/:id', action: :disable_user, controller: 'employees'
-      post '/activate_user/:id', action: :activate_user, controller: 'employees'
-      post '/login', action: :login, controller: 'authentication'
-      post '/register', action: :set_manager, controller: 'employees'
-      put '/update_password/:id', action: :change, controller: 'passwords'
-      post '/forgot_password', action: :forgot, controller: 'passwords'
-      post '/reset_password', action: :reset, controller: 'passwords'
+      resources :returns, only: :index
+      resources :orders, only: [:index, :show, :destroy] do
+        collection do
+          get '/filter_transactions', action: :find, controller: 'orders'
+        end
+        post '/return_item/:line_item_id', action: :return_item, controller: 'returns'
+      end
+      resources :customers
+      resources :employees do
+        collection do
+          post '/disable_user/:id', action: :disable_user, controller: 'employees'
+          post '/activate_user/:id', action: :activate_user, controller: 'employees'
+          post '/register', action: :set_manager, controller: 'employees'
+        end
+        resources :orders, except: [:index, :show, :destory] do
+          resources :line_items, only: [:create, :update, :destroy] do
+            collection do
+              put '/apply_discount/:id', action: :apply_discount, controller: 'line_items'
+            end
+          end
+        end
+      end
+      resources :authentication, except: [:index, :create, :show, :update, :destroy] do
+        collection do
+          post '/login', action: :login, controller: 'authentication'
+        end
+      end
+      resources :passwords, except: [:index, :create, :show, :update, :destroy] do
+        collection do
+          put '/update_password/:id', action: :change, controller: 'passwords'
+          post '/forgot_password', action: :forgot, controller: 'passwords'
+          post '/reset_password', action: :reset, controller: 'passwords'
+        end
+      end
     end
   end
 end
