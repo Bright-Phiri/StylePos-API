756236e08a7a2d5a3f4ae688a3e599ac4141b799
Bright Phiri <bphiri.aki@gmail.com>
2023-10-12 10:43:33 +0200

    update migrations

--- db/migrate/20230122060838_create_employees.rb
+++ db/migrate/20230122060838_create_employees.rb
@@ -3,7 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
     create_table :employees do |t|
       t.string :name
       t.string :job_title
-      t.string :phone_number, index: { unique: true }
+      t.string :phone_number, index: { unique: true, name: 'unique_phone_numbers' }
 
       t.timestamps
     end
