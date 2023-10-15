d9f6fdfc485ea236056388a644160528a83e1ee3
Bright Phiri <bphiri.aki@gmail.com>
2023-10-15 18:19:16 +0200

    add indexing on category

--- db/migrate/20230718070458_add_category_ref_to_items.rb
+++ db/migrate/20230718070458_add_category_ref_to_items.rb
@@ -1,5 +1,5 @@
 class AddCategoryRefToItems < ActiveRecord::Migration[7.0]
   def change
-    add_reference :items, :category, null: false, foreign_key: true
+    add_reference :items, :category, null: false, index: true, foreign_key: true
   end
 end
