1fc055c42a9d5827448eb747ae0e31d452f0a557
Bright Phiri <bphiri.aki@gmail.com>
2023-12-19 11:51:41 +0200

    Fix duplicate index creation issue

--- db/migrate/20230122060401_create_customers.rb
+++ db/migrate/20230122060401_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
       t.string :name
-      t.string :phone_number, index: { unique: true, name: 'unique_phone_numbers' }
+      t.string :phone_number, index: { unique: true, name: 'unique_customer_phone_numbers' }
 
       t.timestamps
     end
