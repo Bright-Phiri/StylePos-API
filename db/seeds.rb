bad61ecc037c90d5aed9897cb27f411dda3c9f0d
Bright Phiri <bphiri.aki@gmail.com>
2023-12-19 12:00:45 +0200

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,32 +2,17 @@
 
 require 'csv'
 
+category = Category.create(name: 'Clothing', description: 'A variety of stylish clothing for every occasion, from casual to formal')
 file_path = Rails.root.join('data.csv')
 csv_data = File.read(file_path)
 csv = CSV.parse(csv_data, headers: false)
-20.times do
-  csv.each do |row|
-    item = Item.new do |i|
-      i.name = row[0]
-      i.price = row[1]
-      i.size = row[2]
-      i.color = row[3]
-      i.category_id = 1
-    end
-    item.save!
+csv.each do |row|
+  item = Item.new do |i|
+    i.name = row[0]
+    i.price = row[1]
+    i.size = row[2]
+    i.color = row[3]
+    i.category_id = category.id
   end
+  item.save!
 end
-
-# 1.times do
-#   items = Item.find_each(batch_size: 50) do |item|
-#     inventory_level = item.create_inventory_level(quantity: 10, reorder_level: 3, supplier: 'Test')
-#     if inventory_level.new_record?
-# #       puts inventory_level.errors.full_messages
-# #     end
-# #   end
-# end
-
-# categories_array = ["Electronics", "Books", "Home Appliances", "Beauty and Personal Care", "Toys and Games", "Automotive", "Art and Craft Supplies", "Sports and Fitness"]
-# categories_array.each do |category|
-#   Category.create(name: category, description: category)
-# end
