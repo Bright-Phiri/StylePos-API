104f9cb81b7c365a0d8672870cda52b2cb0e6dcb
Bright Phiri <bphiri.aki@gmail.com>
2023-12-19 15:53:38 +0200

    fix action cable issues in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,10 +36,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil 
+  # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = [ "http://localhost:8080/", "https://deploy-preview-4--stylepos.netlify.app" ]
-
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.disable_request_forgery_protection = true
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
