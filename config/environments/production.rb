24ebb3dbac5d23c0ea88d05b57d9332333ea2cd9
Bright Phiri <bphiri.aki@gmail.com>
2023-12-19 13:59:39 +0200

    Update Action Cable configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
+  config.action_cable.url = "wss://clothing-retail-api-231a2a43fd56.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['*']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
