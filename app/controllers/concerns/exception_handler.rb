3a4902d5e8c1af0401c947d8f1420ed05aa7e415
Bright Phiri <bphiri.aki@gmail.com>
2023-12-20 21:40:41 +0200

    Improve user-friendliness of ActiveRecord::RecordNotFound error message

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,8 +7,8 @@ module ExceptionHandler
   class InventoryLevelError < StandardError; end
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |exception|
-      render json: { error: exception.message }, status: :not_found
+    rescue_from ActiveRecord::RecordNotFound do
+      render json: { error: "Item not found with barcode #{params[:barcode]}" }, status: :not_found
     end
 
     rescue_from ExceptionHandler::InvalidUsername do |exception|
