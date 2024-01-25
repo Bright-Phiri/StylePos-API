eb5b4f87797d1e37e57ae0ced98c8dc0dc9b1118
Bright Phiri <bphiri.aki@gmail.com>
2024-01-25 11:26:55 +0200

    ☺

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -3,8 +3,6 @@
 class ItemMailer < ApplicationMailer
   def reorder_email
     @item = params[:item]
-    Rails.logger.info("Sending email for item: #{@item.id}")
     mail(to: Employee.where(job_title: 'Store Manager').pluck(:email).first, subject: "Reorder needed for #{@item.name}")
-    Rails.logger.info("Sent")
   end
 end
