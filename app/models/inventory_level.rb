ff2bffe9ba560f711e728947cc942146b69f935a
Bright Issah <bphiri.aki@gmail.com>
2024-04-27 10:10:58 +0200

    update model level validations

--- app/models/inventory_level.rb
+++ app/models/inventory_level.rb
@@ -6,16 +6,20 @@ class InventoryLevel < ApplicationRecord
   validates :supplier, presence: true, allow_blank: true
   validates :quantity, :reorder_level, numericality: { greater_than: 0, only_integer: true }
   validates :quantity, comparison: { greater_than: :reorder_level, message: 'must be greater than reorder level' }, on: :create
-  after_commit :check_inventory_level, on: [:create, :update]
+  after_commit :set_stock_value, :check_inventory_level, on: [:create, :update]
   after_commit { DashboardBroadcastJob.perform_later('inventory') }
 
   def item_name
     item.name
   end
 
+  def set_stock_value
+    self.stock_value = self.quantity * self.item.selling_price
+  end
+
   private
 
   def check_inventory_level
-    ItemMailer.with(item: self.item).reorder_email.deliver_later if self.item.inventory_level.present? && self.item.inventory <= self.item.inventory_level.reorder_level
+    ItemMailer.with(item: self.item).reorder_email.deliver_later if self.item.inventory_level.present? && self.item.inventory <= self.item.reorder_level
   end
 end
