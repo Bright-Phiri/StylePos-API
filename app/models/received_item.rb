a9cf2303763cf95281657e95271049f2883bc6a4
Bright Issah <bphiri.aki@gmail.com>
2024-05-13 08:15:59 +0200

    Update model level validations

--- app/models/received_item.rb
+++ app/models/received_item.rb
@@ -5,13 +5,18 @@ class ReceivedItem < ApplicationRecord
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :batch_number, :supplier, presence: true, allow_blank: true
   validates :cost_price, :selling_price, numericality: { greater_than: 0 }
-  before_save :set_stock_value, :update_inventory
+  before_save :set_stock_value, :add_to_inventory
+  before_destroy :set_stock_value, :remove_from_inventory
 
   def set_stock_value
     self.stock_value = quantity * selling_price
   end
 
-  def update_inventory
+  def add_to_inventory
     self.item.inventory_level.update_attribute(:quantity, self.item.inventory_level.quantity + quantity)
+  end
+
+  def remove_from_inventory
+    self.item.inventory_level.update_attribute(:quantity, self.item.inventory_level.quantity - quantity)
   end
 end
