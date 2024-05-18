3168efb14c3100565dac422d763ea0057c31fc7d
Bright Issah <bphiri.aki@gmail.com>
2024-05-18 08:19:02 +0200

    fix Bug fix on adding received items

--- app/models/received_item.rb
+++ app/models/received_item.rb
@@ -6,7 +6,7 @@ class ReceivedItem < ApplicationRecord
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :batch_number, :supplier, presence: true, allow_blank: true
   validates :cost_price, :selling_price, numericality: { greater_than: 0 }
-  before_save :set_stock_value, :add_to_inventory
+  before_save :set_stock_value, :add_to_inventory, :update_prices
   before_destroy :set_stock_value, :remove_from_inventory
 
   def set_stock_value
@@ -15,6 +15,10 @@ class ReceivedItem < ApplicationRecord
 
   def add_to_inventory
     self.item.inventory_level.update_attribute(:quantity, self.item.inventory_level.quantity + quantity)
+  end
+
+  def update_prices
+    self.item.update_columns(price: cost_price, selling_price:)
   end
 
   def remove_from_inventory
