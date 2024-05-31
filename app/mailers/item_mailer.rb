09a21e4c69d0720a7fb743b31024bede50212d57
Bright Issah <bphiri.aki@gmail.com>
2024-05-31 09:41:12 +0200

    Update system mailers

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class ItemMailer < ApplicationMailer
+  before_action :set_reorder_item
   def reorder_email
-    @item = params[:item]
     mail(to: Employee.where(job_title: 'Store Manager').pluck(:email).first, subject: "Reorder needed for #{@item.name}")
+  end
+
+  private
+
+  def set_reorder_item
+    @item = params[:item]
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  before_action :set_recepient
+
   def password_reset
-    @user = params[:user]
     mail(to: email_address_with_name(@user.email, "#{@user.first_name} #{@user.last_name}"), subject: 'Reset your account password')
+  end
+
+  private
+
+  def set_recepient
+    @user = params[:user]
   end
 end
