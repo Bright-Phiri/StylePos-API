44b0b6d5a3055162313123997fccb167c278a87b
Bright Issah <bphiri.aki@gmail.com>
2024-10-22 10:16:54 +0200

    chore: Update tax rates migration

--- db/migrate/20240318094454_create_tax_rates.rb
+++ db/migrate/20240318094454_create_tax_rates.rb
@@ -1,7 +1,7 @@
 class CreateTaxRates < ActiveRecord::Migration[7.0]
   def change
     create_table :tax_rates do |t|
-      t.string :name
+      t.string :name, index: { unique: true }
       t.decimal :rate
 
       t.timestamps
